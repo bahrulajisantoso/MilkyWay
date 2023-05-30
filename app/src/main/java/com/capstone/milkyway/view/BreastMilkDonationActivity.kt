66028b72da28a5922f5a377ab573e207e69d9ba8
bahrulajisantoso <76810040+bahrulajisantoso@users.noreply.github.com>
2023-05-30 16:27:28 +0700

    add dropdown and get location

--- app/src/main/java/com/capstone/milkyway/view/BreastMilkDonationActivity.kt
+++ app/src/main/java/com/capstone/milkyway/view/BreastMilkDonationActivity.kt
@@ -1,18 +1,81 @@
 package com.capstone.milkyway.view
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.capstone.milkyway.R
 import com.capstone.milkyway.databinding.ActivityBreastMilkDonationBinding
+import com.capstone.milkyway.getAddressName
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class BreastMilkDonationActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBreastMilkDonationBinding
-    
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBreastMilkDonationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         supportActionBar?.hide()
+    }
+
+    private fun getMyLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE_PERMISSIONS
+            )
+            return
+        }
+        fusedLocationClient.lastLocation.addOnSuccessListener {
+            if (it != null) {
+                val address = getAddressName(this@BreastMilkDonationActivity, it.latitude, it.longitude)
+                binding.currentLocTextView.text = address
+            }
+        }
+            .addOnFailureListener {
+                Toast.makeText(
+                    this, "Failed get current location",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+    }
+
+    private fun dropDown() {
+        val religions = resources.getStringArray(R.array.religions)
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, religions)
+        val autocomplete = binding.autoCompleteTextView
+        autocomplete.setAdapter(arrayAdapter)
+
+        autocomplete.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, id ->
+                Toast.makeText(
+                    this@BreastMilkDonationActivity,
+                    religions[position],
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+    }
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 100
     }
 }
--- app/src/main/java/com/capstone/milkyway/view/NeedBreastMilkActivity.kt
+++ app/src/main/java/com/capstone/milkyway/view/NeedBreastMilkActivity.kt
@@ -1,12 +1,23 @@
 package com.capstone.milkyway.view
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.capstone.milkyway.R
 import com.capstone.milkyway.databinding.ActivityNeedBreastMilkBinding
+import com.capstone.milkyway.getAddressName
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class NeedBreastMilkActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityNeedBreastMilkBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +25,57 @@ class NeedBreastMilkActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.hide()
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        binding.locationButton.setOnClickListener { getMyLocation() }
+
+        dropDown()
+    }
+
+    private fun getMyLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE_PERMISSIONS
+            )
+            return
+        }
+        fusedLocationClient.lastLocation.addOnSuccessListener {
+            if (it != null) {
+                val address = getAddressName(this@NeedBreastMilkActivity, it.latitude, it.longitude)
+                binding.currentLocTextView.text = address
+            }
+        }
+            .addOnFailureListener {
+                Toast.makeText(
+                    this, "Failed get current location",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+    }
+
+    private fun dropDown() {
+        val religions = resources.getStringArray(R.array.religions)
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, religions)
+        val autocomplete = binding.autoCompleteTextView
+        autocomplete.setAdapter(arrayAdapter)
+
+        autocomplete.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, id ->
+                Toast.makeText(this@NeedBreastMilkActivity, religions[position], Toast.LENGTH_SHORT)
+                    .show()
+            }
+    }
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 100
     }
 }
