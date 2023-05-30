ebf97542c7d619b19869c96c1d180643ebe45103
bahrulajisantoso <76810040+bahrulajisantoso@users.noreply.github.com>
2023-05-30 16:26:57 +0700

    add get location

--- app/src/main/java/com/capstone/milkyway/Utils.kt
+++ app/src/main/java/com/capstone/milkyway/Utils.kt
@@ -1,7 +1,13 @@
 package com.capstone.milkyway
 
+import android.content.ContentValues
+import android.content.Context
+import android.location.Geocoder
+import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
+import java.io.IOException
+import java.util.*
 
 fun showLoading(progressBar: ProgressBar) {
     progressBar.visibility = View.VISIBLE
@@ -10,4 +16,19 @@ fun showLoading(progressBar: ProgressBar) {
 
 fun hideLoading(progressBar: ProgressBar) {
     progressBar.visibility = View.GONE
+}
+
+fun getAddressName(context: Context, lat:Double, lon:Double): String? {
+    var addressName: String? = null
+    val geocoder = Geocoder(context, Locale.getDefault())
+    try {
+        val list = geocoder.getFromLocation(lat, lon, 1)
+        if (list != null && list.size != 0) {
+            addressName = list[0].getAddressLine(0)
+            Log.d(ContentValues.TAG, "getAddressName: $addressName")
+        }
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+    return addressName
 }
