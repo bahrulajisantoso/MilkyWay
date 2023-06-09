6835440d87ead3ad32acf526e1b669c13683540d
bahrulajisantoso <76810040+bahrulajisantoso@users.noreply.github.com>
2023-06-10 00:29:02 +0700

    make response for api

---
+++ app/src/main/java/com/capstone/milkyway/response/ResponseAddDonor.kt
@@ -0,0 +1,15 @@
+package com.capstone.milkyway.response
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseAddDonor(
+
+	@field:SerializedName("status_code")
+	val statusCode: Int,
+
+	@field:SerializedName("payload")
+	val payload: Int,
+
+	@field:SerializedName("message")
+	val message: String
+)
---
+++ app/src/main/java/com/capstone/milkyway/response/ResponseGetAllDonors.kt
@@ -0,0 +1,54 @@
+package com.capstone.milkyway.response
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseGetAllDonors(
+
+	@field:SerializedName("status_code")
+	val statusCode: Int,
+
+	@field:SerializedName("payload")
+	val payload: List<PayloadItem>,
+
+	@field:SerializedName("message")
+	val message: String
+)
+
+data class PayloadItem(
+
+	@field:SerializedName("address")
+	val address: String,
+
+	@field:SerializedName("role")
+	val role: String,
+
+	@field:SerializedName("phone")
+	val phone: String,
+
+	@field:SerializedName("dietary")
+	val dietary: String,
+
+	@field:SerializedName("insertedAt")
+	val insertedAt: String,
+
+	@field:SerializedName("name")
+	val name: String,
+
+	@field:SerializedName("id")
+	val id: Int,
+
+	@field:SerializedName("userId")
+	val userId: String,
+
+	@field:SerializedName("uuid")
+	val uuid: String,
+
+	@field:SerializedName("age")
+	val age: Int,
+
+	@field:SerializedName("religion")
+	val religion: String,
+
+	@field:SerializedName("updatedAt")
+	val updatedAt: String
+)
