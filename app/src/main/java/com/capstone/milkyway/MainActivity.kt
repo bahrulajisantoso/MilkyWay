267657a9874f869810cbb3744f72858928777905
bahrulajisantoso <76810040+bahrulajisantoso@users.noreply.github.com>
2023-06-14 20:57:56 +0700

    revision

--- app/src/main/java/com/capstone/milkyway/MainActivity.kt
+++ app/src/main/java/com/capstone/milkyway/MainActivity.kt
@@ -55,7 +55,7 @@ class MainActivity : AppCompatActivity() {
                 startActivity(Intent(this@MainActivity, BreastMilkDonationListActivity::class.java))
             }
             careButton.setOnClickListener {
-                startActivity(Intent(this@MainActivity, RouteActivity::class.java))
+                startActivity(Intent(this@MainActivity, BabyCareActivity::class.java))
             }
         }
     }
