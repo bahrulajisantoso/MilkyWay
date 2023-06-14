fa00d03cf70a3e5d063b329515bf63124a464ddf
bahrulajisantoso <76810040+bahrulajisantoso@users.noreply.github.com>
2023-06-14 18:04:35 +0700

    show recommendation

--- app/src/main/java/com/capstone/milkyway/view/RecommendationActivity.kt
+++ app/src/main/java/com/capstone/milkyway/view/RecommendationActivity.kt
@@ -1,16 +1,68 @@
 package com.capstone.milkyway.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.capstone.milkyway.databinding.ActivityRouteBinding
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.capstone.milkyway.UserPreference
+import com.capstone.milkyway.adapter.RecommendAdapter
+import com.capstone.milkyway.databinding.ActivityRecommendationBinding
+import com.capstone.milkyway.loading
+import com.capstone.milkyway.response.ResponseRecommendItem
+import com.capstone.milkyway.viewmodel.RecommendationViewModel
 
 class RecommendationActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityRouteBinding
+    private lateinit var binding: ActivityRecommendationBinding
+    private val viewModel: RecommendationViewModel by viewModels()
+    private lateinit var pref: UserPreference
+    private lateinit var adapter: RecommendAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityRouteBinding.inflate(layoutInflater)
+        binding = ActivityRecommendationBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.rvRecommend.layoutManager = layoutManager
+
+        pref = UserPreference(this)
+
+        getAllRecommends()
+    }
+
+    private fun getAllRecommends() {
+        if (pref.getIdToken() != "") {
+            viewModel.getAllRecommend(
+                pref.getIdToken(),
+                age = 20,
+                dietary = "vegan",
+                religion = "islam",
+                health = "ya",
+                smoking = "ya",
+                blood = "A",
+                location = "Gambir, Jakarta"
+            )
+            viewModel.recommends.observe(this) {
+                adapter = RecommendAdapter(it)
+                binding.rvRecommend.adapter = adapter
+
+                adapter.setOnItemClickCallbackRoute(object :
+                    RecommendAdapter.OnItemClickCallbackRoute {
+                    override fun onItemClicked(recommend: ResponseRecommendItem) {
+                        val intent = Intent(this@RecommendationActivity, RouteActivity::class.java)
+                        intent.putExtra(RouteActivity.LOCATION, recommend.location)
+                        startActivity(intent)
+                    }
+                })
+            }
+        }
+
+        viewModel.isLoading.observe(this) {
+            loading(it, binding.progressBar)
+        }
     }
 }
