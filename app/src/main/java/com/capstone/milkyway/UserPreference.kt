f478800847613baf809a01993808b7d6ae97c8fe
bahrulajisantoso <76810040+bahrulajisantoso@users.noreply.github.com>
2023-06-15 13:31:32 +0700

    pref

--- app/src/main/java/com/capstone/milkyway/UserPreference.kt
+++ app/src/main/java/com/capstone/milkyway/UserPreference.kt
@@ -6,10 +6,11 @@ class UserPreference(context: Context) {
 
     private val preferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
 
-    fun setUser(userId: String, idToken: String) {
+    fun setUser(userId: String, idToken: String, email: String) {
         val editor = preferences.edit()
         editor.putString(USER_ID, userId)
         editor.putString(ID_TOKEN, idToken)
+        editor.putString(EMAIL, email)
         editor.apply()
     }
 
@@ -21,6 +22,10 @@ class UserPreference(context: Context) {
         return preferences.getString(ID_TOKEN, "") ?: ""
     }
 
+    fun getEmail(): String {
+        return preferences.getString(EMAIL, "") ?: ""
+    }
+
     fun logOut() {
         val editor = preferences.edit()
         editor.clear().apply()
@@ -30,5 +35,6 @@ class UserPreference(context: Context) {
         private const val PREFS_NAME = "user_pref"
         private const val USER_ID = "user_id"
         private const val ID_TOKEN = "token"
+        private const val EMAIL = "email"
     }
 }
